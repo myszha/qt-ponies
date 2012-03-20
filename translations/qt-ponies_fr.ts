2b8ec91e157851c9e8158d83c9b20097cd5501b9
Schiwi <mschiwi@googlemail.com>
2012-03-20 20:04:23 +0100

    Added French translation - by Rudy132

---
+++ translations/qt-ponies_fr.ts
@@ -0,0 +1,205 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr">
+<context>
+    <name>ConfigWindow</name>
+    <message>
+        <location filename="../src/configwindow.ui" line="14"/>
+        <source>Qt Ponies</source>
+        <translation>Poney Qt</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="56"/>
+        <source>Add pony</source>
+        <translation>Ajouter un poney</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="210"/>
+        <source>Remove pony</source>
+        <translation>Supprimer un poney</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="253"/>
+        <source>&amp;General</source>
+        <translation>&amp;Général</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="294"/>
+        <source>Toggles if ponies are always on top of other windows</source>
+        <translation>Définit si les poney sont toujours au-dessus des autres fenêtres</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="297"/>
+        <source>&amp;Always on top</source>
+        <translation>Toujours &amp;au-dessus</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="307"/>
+        <source>Bypass the X11 window manager, showing ponies on every desktop on top of every window</source>
+        <translation>Ignorer le gestionnaire de fenêtre X11, affiche les poneys sur chaque bureau au-dessus de chaque fenêtre</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="310"/>
+        <source>Show on every &amp;virtual desktop</source>
+        <translation>Afficher sur chaque bureau &amp;virtuel</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="330"/>
+        <source>Specifies in which directory the pony data and configuration are</source>
+        <translation>Spécifie le dossier où les données et la configuration se trouvent</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="333"/>
+        <source>Pony data di&amp;rectory</source>
+        <translation>Dossier des &amp;données du programme</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="353"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="360"/>
+        <source>Toggle if interactions are to be executed</source>
+        <translation>Définir si les interactions doivent être éxécutées</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="363"/>
+        <source>&amp;Interactions enabled</source>
+        <translation>&amp;Interactions activées</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="373"/>
+        <source>Toggle if debug messages are printed to console</source>
+        <translation>Définir si les messages de déboguage doivent être affichés en console</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="376"/>
+        <source>&amp;Debug messages</source>
+        <translation>Message de &amp;déboguage</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="404"/>
+        <source>S&amp;peech</source>
+        <translation>&amp;Paroles</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="422"/>
+        <source> ms</source>
+        <translation> ms</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="451"/>
+        <source>For how long the speech is to stay on screen</source>
+        <translation>Pour combien de temps les paroles doivent rester à l&apos;écran</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="454"/>
+        <source>&amp;Text delay</source>
+        <translation>Delai du &amp;Texte</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="464"/>
+        <source>Toggles if ponies are to speak</source>
+        <translation>Définit si les poneys doivent parler</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="467"/>
+        <source>&amp;Speech enabled</source>
+        <translation>Parole&amp;s activées</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="496"/>
+        <source>NOT ACTIVE. Toggles the playing of sounds when a pony speaks.</source>
+        <translation>NON ACTIF. Définit le son d&apos;un poney qui parle.</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="499"/>
+        <source>Play s&amp;ounds</source>
+        <translation>Jouer les s&amp;ons</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="519"/>
+        <source>Sound</source>
+        <translation>Son</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="526"/>
+        <source>How frequently ponies speak</source>
+        <translation>Fréquence à laquelle les poneys parlent</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="529"/>
+        <source>Sp&amp;eech probability</source>
+        <translation>Probabilité de parol&amp;e</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="542"/>
+        <source> %</source>
+        <translation>.%</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="572"/>
+        <source>Accept</source>
+        <translation>Accepter</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="579"/>
+        <source>Reset</source>
+        <translation>Réinitialiser</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="597"/>
+        <source>toolBar</source>
+        <translation>Barre d&apos;outils</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="67"/>
+        <source>Open configuration</source>
+        <translation>Ouvrir la configuration</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="68"/>
+        <source>Close application</source>
+        <translation>Fermer le programme</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="76"/>
+        <source>Add ponies</source>
+        <translation>Ajouter des poneys</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="79"/>
+        <source>Active ponies</source>
+        <translation>Poneys actifs</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="81"/>
+        <source>Configuration</source>
+        <translation>Configuration</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="355"/>
+        <source>Select pony data directory</source>
+        <translation>Selectionner le dossier des données des poneys</translation>
+    </message>
+</context>
+<context>
+    <name>Pony</name>
+    <message>
+        <location filename="../src/pony.cpp" line="160"/>
+        <source>Sleeping</source>
+        <translation>Dormant</translation>
+    </message>
+    <message>
+        <location filename="../src/pony.cpp" line="167"/>
+        <source>Remove %1</source>
+        <translation>Supprimer %1</translation>
+    </message>
+    <message>
+        <location filename="../src/pony.cpp" line="168"/>
+        <source>Remove every %1</source>
+        <translation>Tout supprimer %1</translation>
+    </message>
+</context>
+</TS>
