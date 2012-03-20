af591a88c4b2e4c3c086be61f9c9e62bf3f73486
Schiwi <mschiwi@googlemail.com>
2012-03-20 20:11:57 +0100

    Added Greek translation - by d_hoover

---
+++ translations/qt-ponies_el.ts
@@ -0,0 +1,205 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="el_GR">
+<context>
+    <name>ConfigWindow</name>
+    <message>
+        <location filename="../src/configwindow.ui" line="14"/>
+        <source>Qt Ponies</source>
+        <translation>Qt Πόνυ</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="56"/>
+        <source>Add pony</source>
+        <translation>Πρόθεσε πόνι</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="210"/>
+        <source>Remove pony</source>
+        <translation>Αφαίρεσε πόνι</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="253"/>
+        <source>&amp;General</source>
+        <translation type="unfinished">&amp;Γενίκα</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="294"/>
+        <source>Toggles if ponies are always on top of other windows</source>
+        <translation>Εναλλαγή αν τα πόνι είναι πάντα πάνω σε άλλα παράθυρα</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="297"/>
+        <source>&amp;Always on top</source>
+        <translation type="unfinished">Πάντ&amp;α από πάνω</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="307"/>
+        <source>Bypass the X11 window manager, showing ponies on every desktop on top of every window</source>
+        <translation>Παράκαμψε το Χ11 διαχειριστή παραθύρων, δείχνωντας πόνι σε ολά τα επιφάνεια εργασίας από πάνω κάθε παράθυρο</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="310"/>
+        <source>Show on every &amp;virtual desktop</source>
+        <translation type="unfinished">Δείξε σε κάθε εικο&amp;νικό επιφάνεια εργασίας</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="330"/>
+        <source>Specifies in which directory the pony data and configuration are</source>
+        <translation>Προσδιορίζει σε ποίο κατάλογο τα δεδομένα του πόνι και διαμόρωση είναι</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="333"/>
+        <source>Pony data di&amp;rectory</source>
+        <translation>Κα&amp;τάλογος δεδομένων του πόνι</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="353"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="360"/>
+        <source>Toggle if interactions are to be executed</source>
+        <translation>Εναλλαγή αν αλληλεπιδράσεις πρέπει να εκτελεστούν</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="363"/>
+        <source>&amp;Interactions enabled</source>
+        <translation type="unfinished">&amp;Αλληλεπιδράσεις ενεργοποιμένα</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="373"/>
+        <source>Toggle if debug messages are printed to console</source>
+        <translation>Εναλλαγή αν μηνήματα αποσφαλμάτωσης είναι τυποποιμένο στην κονσόλα</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="376"/>
+        <source>&amp;Debug messages</source>
+        <translation type="unfinished">&amp;Μηνύματα αποσφαλμάτωσης</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="404"/>
+        <source>S&amp;peech</source>
+        <translation type="unfinished">Ο&amp;μιλία</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="422"/>
+        <source> ms</source>
+        <translation>χιλ./δευτ</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="451"/>
+        <source>For how long the speech is to stay on screen</source>
+        <translation>Για πόσο καιρό η ομιλία θα μείνει στην οθόνη</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="454"/>
+        <source>&amp;Text delay</source>
+        <translation type="unfinished">&amp;Καθυστέρηση ομιλίας</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="464"/>
+        <source>Toggles if ponies are to speak</source>
+        <translation>Εναλλαγη αν τα πόνι θα μιλούν</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="467"/>
+        <source>&amp;Speech enabled</source>
+        <translation type="unfinished">&amp;Ενεροποίση ομιλίας</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="496"/>
+        <source>NOT ACTIVE. Toggles the playing of sounds when a pony speaks.</source>
+        <translation>ΌΧΙ ΕΡΓΟΝΟΠΟΙΜΈΝΑ. Εναλλαγή η αναπαραγωγή ήχου όταν μιλαέι ένα πόνι.</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="499"/>
+        <source>Play s&amp;ounds</source>
+        <translation type="unfinished">Παίζει η&amp;χους</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="519"/>
+        <source>Sound</source>
+        <translation>Ήχο</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="526"/>
+        <source>How frequently ponies speak</source>
+        <translation>Ποσό συχνά μιλάνε τα πόνι</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="529"/>
+        <source>Sp&amp;eech probability</source>
+        <translation type="unfinished">Πιθανότητες oμ&amp;ιλίας</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="542"/>
+        <source> %</source>
+        <translation> %</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="572"/>
+        <source>Accept</source>
+        <translation>Αποδοχή</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="579"/>
+        <source>Reset</source>
+        <translation>Επαναφορά</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.ui" line="597"/>
+        <source>toolBar</source>
+        <translation>εργαΛεία</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="67"/>
+        <source>Open configuration</source>
+        <translation>Ανοιξέ διαμόρφωση</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="68"/>
+        <source>Close application</source>
+        <translation>Κλείσε εμφαρμογή</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="76"/>
+        <source>Add ponies</source>
+        <translation>Προσθέσε πόνι</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="79"/>
+        <source>Active ponies</source>
+        <translation>Ενεργά πόνι</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="81"/>
+        <source>Configuration</source>
+        <translation>Διαμορφώση</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="355"/>
+        <source>Select pony data directory</source>
+        <translation>Διάλογη κατάλογο δεδομένων πόνι</translation>
+    </message>
+</context>
+<context>
+    <name>Pony</name>
+    <message>
+        <location filename="../src/pony.cpp" line="160"/>
+        <source>Sleeping</source>
+        <translation>Κοιμούνται</translation>
+    </message>
+    <message>
+        <location filename="../src/pony.cpp" line="167"/>
+        <source>Remove %1</source>
+        <translation>Αφάιρεση %1</translation>
+    </message>
+    <message>
+        <location filename="../src/pony.cpp" line="168"/>
+        <source>Remove every %1</source>
+        <translation>Αφάιρεση όλων %1</translation>
+    </message>
+</context>
+</TS>
